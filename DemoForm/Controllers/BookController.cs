dcf2959203bcc1367ff5dce723d3207549c19574
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-01-03 10:10:35 +0100

    create

---
+++ DemoForm/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoForm.Controllers
+{
+    public class BookController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddNew()
+        {
+            var model = new ViewModels.BookViewModel();
+            model.Language = "Svenska";
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddNew(ViewModels.BookViewModel model)
+        {
+            if(string.IsNullOrEmpty(model.Title))
+            {
+                return View(model);
+            }
+
+
+            ///Save to database
+            model.IsSaved = true;
+            return View(model);
+
+            //return View(model);
+        }
+
+    }
+}
---
+++ DemoForm/ViewModels/BookViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoForm.ViewModels
+{
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublishedYear { get; set; }
+        public string Publisher { get; set; }
+        public string Language { get; set; }
+
+        public bool IsSaved { get; set; }
+    }
+}
